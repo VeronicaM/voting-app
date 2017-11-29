6b8be2854a2b5020c5298c0f9d71cb9bec477a31
Mihai <mihaiveronica@ymail.com>
2017-11-29 16:35:54 +0200

    fixing Twitter redirect bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  serverURL: 'http://127.0.0.1'
+  serverURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1' : process.env.DOMAIN
 };
