d9348a9bc316d6ec654fb071729630952add506b
Mihai <mihaiveronica@ymail.com>
2017-12-08 13:02:28 +0200

    testing to see if the ipify package can fix the dynamic address problem on heroku

--- server/api/polls/poll.controller.js
+++ server/api/polls/poll.controller.js
@@ -4,6 +4,7 @@ var jsonpatch = require('fast-json-patch');
 var Poll = require('./poll.model');
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
+const ipify = require('ipify');
 
 function respondWithResult(res, statusCode) {
     statusCode = statusCode || 200;
@@ -66,24 +67,23 @@ exports.show = function(req, res) {
     Poll.findById(req.params.id, function(err, foundPoll) {
         if (err) return res.status(500).send('Something went wrong');
         if (!foundPoll) return res.status(404).send('Poll not found!');
-
-        var id = getUserIP(req);
-
-        if (id.user !== 'anonymous') {
-            var filteredValues = foundPoll.votes.filter(function(el) {
-                return id.user !== undefined ?
-                    el.id.IP === id.IP || el.id.user === id.user :
-                    el.id.IP === id.IP;
-            });
-            var votedValue = filteredValues[0] ? filteredValues[0].value : null;
-            if (votedValue) {
-                sendVotedPoll(res, foundPoll, votedValue);
+        var id = getUserIP(req).then(function(id) {
+            if (id.user !== 'anonymous') {
+                var filteredValues = foundPoll.votes.filter(function(el) {
+                    return id.user !== undefined ?
+                        el.id.IP === id.IP || el.id.user === id.user :
+                        el.id.IP === id.IP;
+                });
+                var votedValue = filteredValues[0] ? filteredValues[0].value : null;
+                if (votedValue) {
+                    sendVotedPoll(res, foundPoll, votedValue);
+                } else {
+                    res.status(200).send(foundPoll);
+                }
             } else {
                 res.status(200).send(foundPoll);
             }
-        } else {
-            res.status(200).send(foundPoll);
-        }
+        });
     });
 };
 // Creates a new Poll in the DB
@@ -101,55 +101,53 @@ exports.create = function(req, res) {
 
 // Updates an existing Poll in the DB
 exports.update = function(req, res) {
-    var id = getUserIP(req);
-
-    var voteValue = req.body.voteValue;
-    var vote = { id: id, value: voteValue };
-    return Poll.findOneAndUpdate({
-            $and: [{
-                    $or: [
-                        { _id: req.params.id },
-                        { 'votes.id.userId': { $ne: id.userId } },
-                    ],
-                },
-                { $or: [{ _id: req.params.id }, { 'votes.id.IP': { $ne: id.IP } }] },
-            ],
-        }, { $push: { votes: vote } }, { upsert: true },
-        function(error, poll) {
-            if (poll) {
-                var addNewOption = poll.options.indexOf(voteValue) == -1;
-                if (addNewOption) {
-                    poll.options.push(voteValue);
-                    poll.save(function(err, savedPoll) {
-                        if (err) res.status(500).send('Something went wrong');
-                        sendVotedPoll(res, savedPoll, voteValue);
-                    });
+    getUserIP(req).then(function(id) {
+        var voteValue = req.body.voteValue;
+        var vote = { id: id, value: voteValue };
+        return Poll.findOneAndUpdate({
+                $and: [{
+                        $or: [
+                            { _id: req.params.id },
+                            { 'votes.id.userId': { $ne: id.userId } },
+                        ],
+                    },
+                    { $or: [{ _id: req.params.id }, { 'votes.id.IP': { $ne: id.IP } }] },
+                ],
+            }, { $push: { votes: vote } }, { upsert: true },
+            function(error, poll) {
+                if (poll) {
+                    var addNewOption = poll.options.indexOf(voteValue) == -1;
+                    if (addNewOption) {
+                        poll.options.push(voteValue);
+                        poll.save(function(err, savedPoll) {
+                            if (err) res.status(500).send('Something went wrong');
+                            sendVotedPoll(res, savedPoll, voteValue);
+                        });
+                    } else {
+                        poll.votes.push(vote);
+                        sendVotedPoll(res, poll, vote);
+                    }
                 } else {
-                    poll.votes.push(vote);
-                    sendVotedPoll(res, poll, vote);
+                    res.status(404).send('You have already voted!');
                 }
-            } else {
-                res.status(404).send('You have already voted!');
             }
-        }
-    );
+        );
+    });
 };
 
 function getUserIP(req) {
-    var id = new Object();
-    if (req.body.user) {
-        //if signed in user, get user id
-        id.userId = req.body.user._id;
-    }
-    //else get PC's IP Address
-    try {
-        console.log(req.ip);
-        id.IP = req.ip || req.connection.remoteAddress;
-    } catch (ex) {
-        id.user = 'anonymous';
-        console.log(ex);
-    }
-    return id;
+    return new Promise(function(resolve, reject) {
+        ipify().then(ip => {
+            var id = new Object();
+            if (req.body.user) {
+                //if signed in user, get user id
+                id.userId = req.body.user._id;
+            }
+            console.log(ip);
+            id.IP = ip;
+            resolve(id);
+        });
+    });
 }
 
 function sendVotedPoll(res, poll, voteValue) {
