383ffc85aed8a3d4afe64b193cd73c85b7d243c0
Mihai <mihaiveronica@ymail.com>
2018-02-02 10:09:16 +0200

    add delete poll on poll details

--- src/app/pages/poll-details/poll-details.component.ts
+++ src/app/pages/poll-details/poll-details.component.ts
@@ -12,6 +12,7 @@ import { PollsStoreService } from '../../common';
 export class PollDetailsComponent implements OnInit {
   currentPoll$: any;
   id: string;
+  isMyPoll: boolean;
   vote = { selected: '', custom: '' };
   private paramsSubscription;
   constructor(
@@ -24,6 +25,16 @@ export class PollDetailsComponent implements OnInit {
     this.id = this.route.snapshot.params.id;
     this.pollService.get(this.id);
     this.currentPoll$ = this.pollService.currentPoll$;
+    this.pollService.getMyPolls();
+    this.pollService.myPolls$.subscribe(mypolls => {
+      if (mypolls.length !== 0) {
+        if (mypolls.createdPolls.length !== 0) {
+          this.isMyPoll =
+            mypolls.createdPolls.filter(poll => poll._id === this.id).length !==
+            0;
+        }
+      }
+    });
   }
   makeChoice(value) {
     this.vote.selected = value;
@@ -37,5 +48,12 @@ export class PollDetailsComponent implements OnInit {
     const twtLink =
       'http://twitter.com/home?status=' + encodeURIComponent(textToTweet);
     window.open(twtLink, '_blank');
+  }
+  deletePoll(id) {
+    const deletePoll = confirm('Are you sure you want to delete this poll ?');
+    if (deletePoll) {
+      this.pollService.deletePoll(id);
+      this.router.navigate(['dashboard']);
+    }
   }
 }
